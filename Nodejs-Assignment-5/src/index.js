91c953ecf68eccfcbf2408d6879f191a983988c0
deepakcit <110526131+deepakcit@users.noreply.github.com>
2023-02-21 19:55:40 +0530

    Add files via upload

---
+++ Nodejs-Assignment-5/src/index.js
@@ -0,0 +1,32 @@
+// var http = require("http");
+
+// const httpServer = http.createServer(handleServer);
+
+
+// function handleServer(req, res) {
+  
+// }
+
+// module.exports = httpServer;
+var http = require("http");
+
+const httpServer = http.createServer(handleServer);
+
+function handleServer(req, res) {
+  if (req.url === "/welcome") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Welcome to Dominos!");
+  } else if (req.url === "/contact") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({ phone: '18602100000', email: 'guestcaredominos@jublfood.com' }));
+  } else {
+    res.writeHead(404);
+    res.end("Route not found");
+  }
+}
+
+httpServer.listen(8081, () => {
+  console.log("Server is running on port 8081");
+});
+
+module.exports = httpServer;
